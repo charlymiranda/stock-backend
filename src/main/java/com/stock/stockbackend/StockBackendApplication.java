8a85b683f6d63fcfdfe278cc8025c070f2e8978c
Miranda, Carlos <mirandacarlosmi@gmail.com>
2025-04-20 10:45:11 -0300

    fix flyway

--- src/main/java/com/stock/stockbackend/StockBackendApplication.java
+++ src/main/java/com/stock/stockbackend/StockBackendApplication.java
@@ -2,8 +2,12 @@ package com.stock.stockbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.stock.stockbackend.repository")
+@EntityScan("com.stock.stockbackend.model")
 public class StockBackendApplication {
 
     public static void main(String[] args) {
